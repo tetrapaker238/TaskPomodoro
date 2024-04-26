5d073491fd92195bc4757465555f72168584d468
tetrapaker238 <alejandro.salas@live.cl>
2024-04-25 23:01:16 -0400

    [feat] Added Button and Text with timer logic using CountDownTimer

--- app/src/main/java/com/example/taskpomodoro/MainActivity.kt
+++ app/src/main/java/com/example/taskpomodoro/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.taskpomodoro
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.taskpomodoro.ui.theme.TaskPomodoroTheme
@@ -22,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    TimeDisplay()
                 }
             }
         }
@@ -30,17 +39,52 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun TimeDisplay() {
+    ButtonAndTime(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(align = Alignment.Center)
     )
 }
 
-@Preview(showBackground = true)
+@Composable
+fun ButtonAndTime(modifier: Modifier = Modifier) {
+
+    var timeText by rememberSaveable { mutableStateOf("25:00") }
+    val timer = object: CountDownTimer(1000*60*25, 1000) {
+        override fun onTick(millisUntilFinished: Long) {
+            val minutes = (millisUntilFinished / (1000 * 60)).toInt()
+            val seconds = ((millisUntilFinished - (minutes * 1000 * 60)) / 1000).toInt()
+            val strMinutes = if (minutes >= 10) minutes.toString() else "0$minutes"
+            val strSeconds = if (seconds >= 10) seconds.toString() else "0$seconds"
+            timeText = "$strMinutes:$strSeconds"
+        }
+
+        override fun onFinish() {
+            return
+        }
+
+    }
+
+    Column (
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = timeText,
+        )
+        Button(onClick = { timer.start() }) {
+            Text(text = "Start pomodoro")
+        }
+    }
+}
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
 @Composable
 fun GreetingPreview() {
     TaskPomodoroTheme {
-        Greeting("Android")
+        TimeDisplay()
     }
 }
