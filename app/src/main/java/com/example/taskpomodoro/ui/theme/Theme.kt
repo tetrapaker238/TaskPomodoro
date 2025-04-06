684000b80f421e867c7687c1dfe573e1844d751b
tetrapaker238 <alejandro.salas@live.cl>
2025-04-06 18:50:22 -0400

    build: Update gradle version and packages

--- app/src/main/java/com/example/taskpomodoro/ui/theme/Theme.kt
+++ app/src/main/java/com/example/taskpomodoro/ui/theme/Theme.kt
@@ -111,7 +111,7 @@ fun TaskPomodoroTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+             window.statusBarColor = colorScheme.primary.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
