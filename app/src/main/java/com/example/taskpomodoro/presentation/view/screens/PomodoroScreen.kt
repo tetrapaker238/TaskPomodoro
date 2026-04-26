11746737480e6994210e397b4ab4f13573614c9e
tetrapaker238 <alejandro.salas@live.cl>
2026-04-26 16:59:24 -0400

    refactor: modify top bar and bottom bar for initial pomodoro screen

--- app/src/main/java/com/example/taskpomodoro/presentation/view/screens/PomodoroScreen.kt
+++ app/src/main/java/com/example/taskpomodoro/presentation/view/screens/PomodoroScreen.kt
@@ -6,22 +6,34 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.taskpomodoro.R
 import com.example.taskpomodoro.data.dataclasses.PomodoroUiState
 import com.example.taskpomodoro.presentation.view.composables.DialogWithForm
 import com.example.taskpomodoro.presentation.view.composables.FinishedPomodoroText
+import com.example.taskpomodoro.presentation.view.composables.NavigationButtons
 import com.example.taskpomodoro.presentation.view.composables.TaskSection
 import com.example.taskpomodoro.presentation.view.composables.TimeDisplay
-import com.example.taskpomodoro.presentation.view.composables.Toolbar
 import com.example.taskpomodoro.presentation.viewmodel.PomodoroViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PomodoroScreen(
     modifier: Modifier = Modifier, pomodoroViewModel: PomodoroViewModel = viewModel(factory = PomodoroViewModel.Factory)
@@ -30,9 +42,29 @@ fun PomodoroScreen(
     // pomodoroViewModel Ui State the collectAsState getter
     Scaffold(
         topBar = {
-            Toolbar(
-                onSettingsClick = { pomodoroViewModel.toggleDialog() }
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                ),
+                title = {Text("Pomodoro timer")},
+                actions = {
+                    IconButton(onClick = { pomodoroViewModel.toggleDialog() }) {
+                        Icon(
+                            painter = painterResource(R.drawable.settings_24px),
+                            contentDescription = "Configure pomodoro",
+                            tint = MaterialTheme.colorScheme.onPrimary,
+                            modifier = Modifier.size(48.dp)
+                        )
+                    }
+                }
+
             )
+        },
+        bottomBar = {
+            BottomAppBar {
+                NavigationButtons()
+            }
         },
         modifier = modifier
             .fillMaxSize(),
